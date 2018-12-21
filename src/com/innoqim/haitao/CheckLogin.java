3ceec50a58c813b5cb858906e95d8e05ec434df0
haitao <allen.qiht119@gmail.com>
2018-12-21 12:03:15 -0500

    add Jenkinsfile

--- src/com/innoqim/haitao/CheckLogin.java
+++ src/com/innoqim/haitao/CheckLogin.java
@@ -2,13 +2,28 @@ package com.innoqim.haitao;
 
 import static org.junit.Assert.*;
 
+import java.util.Objects;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CheckLogin {
 
+	//Declare a Webdriver variable
+    public WebDriver driver;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testLoginByChrome() {   
+		System.setProperty("webdriver.chrome.driver","/Users/haitao/util/chromedriver");   
 
+		driver = new ChromeDriver();    
+		
+		driver.get("http://www.innoqim.com/login.html");
+ 
+		//click login
+		driver.findElement(By.xpath("//html/body/section[3]/div/div[1]/div[1]/a")).click(); 
+
+		}
 }
